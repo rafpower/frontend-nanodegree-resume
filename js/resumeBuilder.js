fc49f8753956a99333a18f8108ebb775c58500fb
unknown <rafpower@users.noreply.github.com>
2015-01-04 22:13:18 +0000

    Add variables to resumeBuilder.js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,31 @@
+var name = "Tom Hacker"
+var role = "Web developer"
+var welcome = "Hello and welcome to my bio"
+var skills = ["swimming", "skiing", "flying"]
+
+//var formattedname = HTMLheaderName.replace("%data%", name)
+//var formattedrole = HTMLheaderRole.replace("%data%", role)
+
+var bio = {
+	"name": name,
+	"role": role,
+	"contact-info": "email",
+	"picture-url": "none",
+	"welcome-message": welcome,
+	"skills": skills
+};
+
+var work = {}
+work.position = "analyst";
+
+var projects = {};
+
+var education = {}
+education["name"] = "Bristol Uni";
+
+//$("#header").prepend(formattedname);
+
+//$("#main").append(bio.skills);
+
+//$("#main").append(work["position"]);
+//$("#main").append(education.name);
